ca08e60d86d6bd6f9dbfddcf203b358358cf4f3b
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-11-09 22:36:51 +0100

    Esercice 63 done (tecnicaly). Start the corrections

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -24,6 +24,8 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
+
+    /* 
     public static void main(final String... args) {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
@@ -31,4 +33,42 @@ public final class LaunchApp {
         app.addView(new DrawNumberSwingView());
         app.addView(new DrawNumberStdoutView());
     }
+     */
+    
+    public static void main(final String... args) {
+        //final Class c1 = Class.forName()
+        ////final Class<DrawNumberImpl> Model = DrawNumberImpl.class;
+        ////final var model = Model.newInstance();
+        
+        //final DrawNumberController app = new DrawNumberControllerImpl(model);
+        ////final Class<DrawNumberControllerImpl> App = DrawNumberControllerImpl.class;
+        ////final DrawNumberController app = App.newInstance();
+        //app.new
+        
+        //app.addView(new DrawNumberSwingView());
+        //app.addView(new DrawNumberSwingView());
+        //app.addView(new DrawNumberStdoutView());
+        //final String packView = "it.unibo.mvc.view";
+
+
+        final var model = new DrawNumberImpl();
+        final DrawNumberController app = new DrawNumberControllerImpl(model);
+        
+        final Class<DrawNumberSwingView> swingV = DrawNumberSwingView.class;
+        final Class<DrawNumberStdoutView> stdoutV = DrawNumberStdoutView.class;
+
+        for(int i=0;i<3;i++){
+            try {
+                app.addView(swingV.newInstance());
+                app.addView(stdoutV.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+           
+        }
+    
+    }
+    
+    
 }
